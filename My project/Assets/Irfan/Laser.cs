ef343c37e00f6b23268ac9623f66e7e57455a7c9
Res2dinv <131385158+Res2dinv@users.noreply.github.com>
2025-02-24 00:44:52 +0700

    Musuh Nembak

---
+++ My project/Assets/Irfan/Laser.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float speed = 20f;
+    public float lifetime = 5;
+
+    private Vector3 moveDirection;
+
+    public void SetDirection(Vector3 direction)
+    {
+        
+        moveDirection = (direction - transform.position).normalized;
+        transform.rotation = Quaternion.LookRotation(moveDirection);
+    }
+
+    void Update()
+    {
+        transform.position += moveDirection * speed * Time.deltaTime; // Move laser forward
+    }
+
+    void Start()
+    {
+        if (gameObject.name.Contains("(Clone)"))
+        {
+            Destroy(gameObject, lifetime);
+        } // Destroy laser after a set time
+    }
+}
