38366b1fc773c648ec954b185d50dda4087e4e45
D-GT <19624184@mahasiswa.itb.ac.id>
2025-02-24 23:45:28 +0700

    Added SFX + BGM

---
+++ My project/Assets/Almer/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] Canvas settingsScreen;
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        settingsScreen.gameObject.SetActive(false);
+    }
+    public void StartGame() 
+    {
+        SceneManager.LoadScene("Tutorial");
+        Time.timeScale = 1;
+    }
+
+    public void BackToMenu() 
+    {
+        SceneManager.LoadScene("Starting Screen");
+        Time.timeScale = 1;
+    }
+
+    public void ToSettings()
+    {
+        settingsScreen.gameObject.SetActive(true);
+    }
+
+    public void QuitSettings()
+    {
+        settingsScreen.gameObject.SetActive(false);
+    }
+}
